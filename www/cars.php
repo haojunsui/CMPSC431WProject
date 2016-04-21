383e1083f233c97ab59a85a33e5210be0c59a41d
Haojun Sui <shjnyr@me.com>
2016-04-21 10:29:21 -0400

    Adds readable comments to cars.php

--- www/cars.php
+++ www/cars.php
@@ -4,6 +4,7 @@
 
 <head>
 	<?php
+		// Make mongodb connection
 		$m = new MongoClient();
 		$db = $m->selectDB("biglegcarry");
 		$maker_col = new MongoCollection($db, "maker");
@@ -13,9 +14,11 @@
 		$models = array();
 		array_push($models, "Any");
 
+		// Get maker and model from url
 		$maker = $_GET["maker"];
 		$model = $_GET["model"];
 
+		// Find makers from mongodb
 		$cursor = $maker_col->find();
 
 		foreach ($cursor as $obj) {
@@ -23,6 +26,7 @@
 		}
 
 		if ($maker != "Any") {
+			// Find models from mongodb
 			$cursor = $model_col->find(array("maker" => $maker));
 
 			foreach ($cursor as $obj) {
@@ -35,36 +39,47 @@
 
 	<script type="text/javascript">
 		function changed() {
+			// Refresh page with new maker and new model
 			var maker_sel = document.getElementById("maker");
 			maker_sel = maker_sel.options[maker_sel.selectedIndex].value;
 			var model_sel = document.getElementById("model");
 			model_sel = model_sel.options[model_sel.selectedIndex].value;
 
-			location.href = "cars.php?maker=" + maker_sel + "&model=" + model_sel;
+			if (maker_sel != "Any")
+				location.href = "cars.php?maker=" + maker_sel + "&model=" + model_sel;
+			else
+				location.href = "cars.php?maker=Any&model=Any";
 		}
 
 		function pop_dropdown() {
+			// Get makers array and models array from php
 			var makers = <?php echo json_encode($makers);?>;
 			var models = <?php echo json_encode($models);?>;
 
 			var maker_drop = document.getElementById("maker");
 
+			// Populate makers dropdown list
 			for (var i = 0; i < makers.length; i++) {
 				opt = document.createElement('option');
 				opt.text = makers[i];
 				opt.value = makers[i];
 				maker_drop.options.add(opt);
+				// Set makers dropdown currently selected
 				if (maker_drop.options[i].value == <?php echo json_encode($maker);?>)
 					maker_drop.options[i].selected = true;
 			}
 
 			var model_drop = document.getElementById("model");
 
+			// Populate models dropdown list
 			for (var i = 0; i < models.length; i++) {
 				opt = document.createElement('option');
 				opt.text = models[i];
 				opt.value = models[i];
 				model_drop.options.add(opt);
+				// Set models dropdown currently selected
+				if (model_drop.options[i].value == <?php echo json_encode($model);?>)
+					model_drop.options[i].selected = true;
 			}
 		}
 	</script>
