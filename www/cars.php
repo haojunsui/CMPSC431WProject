9c41d1814e33e224f01c3b33980bfbad404faf74
Haojun Sui <shjnyr@me.com>
2016-04-21 10:14:05 -0400

    Update Model Selection

--- www/cars.php
+++ www/cars.php
@@ -6,28 +6,65 @@
 	<?php
 		$m = new MongoClient();
 		$db = $m->selectDB("biglegcarry");
-		$collection = new MongoCollection($db, "maker");
-
-		$cursor = $collection->find();
-
+		$maker_col = new MongoCollection($db, "maker");
+		$model_col = new MongoCollection($db, "model");
 		$makers = array();
+		array_push($makers, "Any");
+		$models = array();
+		array_push($models, "Any");
+
+		$maker = $_GET["maker"];
+		$model = $_GET["model"];
+
+		$cursor = $maker_col->find();
 
 		foreach ($cursor as $obj) {
 			array_push($makers, $obj["maker"]);
 		}
+
+		if ($maker != "Any") {
+			$cursor = $model_col->find(array("maker" => $maker));
+
+			foreach ($cursor as $obj) {
+				foreach ($obj["model"] as $t) {
+					array_push($models, $t);
+				}
+			}
+		}
 	?>
 
 	<script type="text/javascript">
-		function pop_makers() {
+		function changed() {
+			var maker_sel = document.getElementById("maker");
+			maker_sel = maker_sel.options[maker_sel.selectedIndex].value;
+			var model_sel = document.getElementById("model");
+			model_sel = model_sel.options[model_sel.selectedIndex].value;
+
+			location.href = "cars.php?maker=" + maker_sel + "&model=" + model_sel;
+		}
+
+		function pop_dropdown() {
 			var makers = <?php echo json_encode($makers);?>;
+			var models = <?php echo json_encode($models);?>;
 
 			var maker_drop = document.getElementById("maker");
 
-			for(var i = 0; i < makers.length; i++) {
-				var opt = document.createElement('option');
+			for (var i = 0; i < makers.length; i++) {
+				opt = document.createElement('option');
 				opt.text = makers[i];
 				opt.value = makers[i];
 				maker_drop.options.add(opt);
+				if (maker_drop.options[i].value == <?php echo json_encode($maker);?>)
+					maker_drop.options[i].selected = true;
+			}
+
+			var model_drop = document.getElementById("model");
+
+			for (var i = 0; i < models.length; i++) {
+				opt = document.createElement('option');
+				opt.text = models[i];
+				opt.value = models[i];
+				model_drop.options.add(opt);
 			}
 		}
 	</script>
@@ -35,14 +72,14 @@
 
 <title>Our Cars - BigLegCarry</title>
 
-<body onLoad="pop_makers();">
+<body onLoad="pop_dropdown();">
 	<div style="text-align:center">
 		<header>
 			<h1>BigLegCarry</h1>
 			<table border="1" align="center" width="100%">
 				<tr>
 					<td><a href="index.php">Home</a></td>
-					<td><a href="cars.php">Cars</a></td>
+					<td><a href="cars.php?maker=Any&model=Any">Cars</a></td>
 					<td><a href="dealers.php">Dealers</a></td>
 					<td><a href="about.php">About Us</a></td>
 					<td>
@@ -58,16 +95,12 @@
 		<table border="0" width="100%">
 			<tr>
 				<td>
-					Model:
-					<select>
-						<option value="any">Any</option>
-					</select>
+					Maker:
+					<select id="maker" onchange="changed();"></select>
 				</td>
 				<td>
-					Maker:
-					<select id="maker">
-						<option value="any">Any</option>
-					</select>
+					Model:
+					<select id="model" onchange="changed();"></select>
 				</td>
 			</tr>
 		</table>
