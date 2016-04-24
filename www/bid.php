08c82d7659781623208cef63d7dbbad666df9e44
Haowang5128 <Haowang5128@gmail.com>
2016-04-23 20:28:00 -0400

    不知道对不对啊写的 不知道怎么把数据get过来。。

---
+++ www/bid.php
@@ -0,0 +1,25 @@
+<?php
+$connection = new MongoClient();
+$db = $connection -> biglegcarry;
+$col_bid = $connection -> bid;
+
+$bid_user = $bid_vin = $bid_topprice = $bid_newprice'';
+
+//get data from when user click bid button?
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+	$bid_user = $_POST['username'];
+	$bid_vin = $_POST['vin'];
+	$bid_topprice = $_POST['price showed on page'];
+	$bid_newprice = $_POST['user entered price']
+
+	if ($bid_topprice > $bid_newprice) {
+		echo "Please enter a new price higher than current bidding price"
+	}
+
+	else {
+		$col_bid -> update(array("username" => $bid_user, "price" => $bid_newprice));
+		echo "Bid successfully"
+	}
+}
+?>
