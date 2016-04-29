10e105bb496f318749fccbc1f359e62096fe6449
YuanpeiDeng <dengyuanpei@gmail.com>
2016-04-29 00:30:20 -0400

    buy.php

---
+++ www/buy.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+	<link rel="stylesheet" type="text/css" href="css/default.css">
+</head>
+
+<title>Buying - BigLegCarry</title>
+
+<body onload="pop_dobdrop();">
+	<h1>BigLegCarry</h1>
+
+	<div class="navigation_bar">
+		<ul>
+			<li><a href="index.php">Home</a></li>
+			<li><a href="cars.php?maker=Any&model=Any&color=Any">Cars</a></li>
+			<li><a href="about.php">About Us</a></li>
+			<li id="search">
+				<input type="text" id="search_cars" placeholder="Type to search">
+			</li>
+			<li><a href="login.php">Log in / Sign up</a></li>
+		</ul>
+	</div>
+	<br>
+	<form style="text-align:center;">
+		You are Buying this car<br>
+		<input type="text" id="firstname" placeholder="First name"><br>
+		<input type="text" id="lastname" placeholder="Last name"><br>
+		<input type="text" id="address" placeholder="address"><br>
+		<input type="text" id="cardNo" placeholder="cardNo"><br>
+		<input type="text" id="CVV" placeholder="CVV"><br>
+	</form>
+	<br>
+	<div class="footer">
+		<p class="footer-links">
+			<a href="index.php">Home</a>
+			|
+			<a href="cars.php?maker=Any&model=Any&color=Any">Cars</a>
+			|
+			<a href="about.php">About Us</a>
+		</p>
+
+		<p class="footer-company-name">BigLegCarry &copy; 2016</p>
+	</div>
+</body>
+
+<script>
+</script>
+
+</html>
