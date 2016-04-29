c5ace81814955ea497aee03ce077383f5a904302
YuanpeiDeng <dengyuanpei@gmail.com>
2016-04-29 00:02:59 -0400

    no message

--- www/index.php
+++ www/index.php
@@ -23,7 +23,7 @@
 		</ul>
 	</div>
 	<br>
-	<img src="/www/Screen Shot 2016-04-28 at 11.09.29 PM" alt="image" style="width:128px;height:128px;">
+	<img src="/www/Screen Shot 2016-04-28 at 11.09.29 PM.png" alt="image" style="width:128px;height:128px;">
 	<br>
 	<div class="footer">
 		<p class="footer-links">
